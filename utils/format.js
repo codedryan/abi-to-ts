056fc489e6a48cde25445fde2ef9bd33a5e3ed81
Ryan <145731943+codedryan@users.noreply.github.com>
2024-06-05 16:05:26 +0800

    perf: simplified code

--- utils/format.js
+++ utils/format.js
@@ -1,6 +1,7 @@
 "use strict";
 
 const { getAddress } = require("viem");
+const { parseAbi, formatAbi: formatAbiT } = require("abitype");
 
 function formatNetworks(networks) {
   return Object.keys(networks ?? {}).reduce((prev, cur) => {
@@ -11,25 +12,7 @@ function formatNetworks(networks) {
 }
 
 function formatAbi(json) {
-  return (json.abi ?? []).map((abiItem) => {
-    if (abiItem.inputs) {
-      for (let i = 0; i < abiItem.inputs.length; i++) {
-        if (abiItem.inputs[i].internalType) {
-          delete abiItem.inputs[i].internalType;
-        }
-      }
-    }
-
-    if (abiItem.outputs) {
-      for (let i = 0; i < abiItem.outputs.length; i++) {
-        if (abiItem.outputs[i].internalType) {
-          delete abiItem.outputs[i].internalType;
-        }
-      }
-    }
-
-    return abiItem;
-  });
+  return parseAbi(formatAbiT(json.abi ?? []));
 }
 
 module.exports = { formatNetworks, formatAbi };
